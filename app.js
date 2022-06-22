e9c1102c80231c50785c13b4202e5f7933160033
Simon Oladeji <simmax456@gmail.com>
2022-06-22 03:32:44 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+let firstName = "Simon", 
+    lastName = "Oladeji",
+    height = "6ft",
+    country = "Nigeria",
+
+    fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+alert(fullName + " is " + height + " tall and lives in " + country + ".")
+alert("Please check the console.")
